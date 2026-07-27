351e89df114b8a596ba3b218e8abbc62a09b2091
[ramshasalim] <[ramshasalim15@gmail.com]>
2026-07-28 04:13:08 +0530

    util files setup

--- src/db/index.js
+++ src/db/index.js
@@ -11,7 +11,6 @@ const connectDB = async () => {
     );
   } catch (error) {
     console.log("MONGODB connection error", error);
-    process.exit(1);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -5,4 +5,17 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    const server = app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port :${process.env.PORT}`);
+    });
+    server.on("error", (err) => {
+      console.log("error:", err);
+      throw err;
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB Connection failed", err);
+    process.exit(1);
+  });
